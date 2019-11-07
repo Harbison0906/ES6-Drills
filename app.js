78a9b40d4f25a810616fe4b3ba1f5414091fb74f
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-11-06 20:25:34 -0600

    writing code to practice template literals, default parameters, and arrow functions

---
+++ app.js
@@ -0,0 +1,11 @@
+let favMovie = (name = 'World', movie = 'The Room') => console.log(`My name is ${name} and my favorite movie is ${movie}.`);
+
+favMovie();
+
+const getFirstName = (name) => {
+  let firstName = name.split(' ');
+  console.log(firstName[0]);
+}
+getFirstName('Joshua Harbison');
+
+
